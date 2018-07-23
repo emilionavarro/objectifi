f9c905d0abb2f8a5280f5f5c08261545ee8fb6a2
Ben Brooks <ben.brooks@RecordLion.com>
2018-07-23 10:55:54 -0500

    Rmoved /index from import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {
-  ItemsListComponent,
-  ItemListResolver,
-  ItemDetailsComponent
-} from './items/index';
+import {ItemsListComponent, ItemListResolver, ItemDetailsComponent} from './items/';
 
 const appRoutes: Routes = [
   { path: 'items', component: ItemsListComponent, resolve: { items: ItemListResolver } },
