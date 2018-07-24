a9dab0d44b3ce9e759a8d5ed763d223d0fefd8e1
philip <jellycore@outlook.com>
2018-07-23 19:19:35 -0500

    Fix to me not realizing how to merge...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-=======
->>>>>>> f9c905d0abb2f8a5280f5f5c08261545ee8fb6a2
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -18,12 +15,9 @@ import {
   ItemListResolver
 } from './items/index';
 import { ConsumerComponent } from './items/consumer/consumer.component';
-<<<<<<< HEAD
 import { MyStringComponent } from './items/inputs/my-string/my-string.component';
-=======
 import { TodoListComponent } from './items/todo-list/todo-list.component';
 import { HistoricalListComponent } from './items/historical-list/historical-list.component';
->>>>>>> f9c905d0abb2f8a5280f5f5c08261545ee8fb6a2
 
 @NgModule({
   declarations: [
@@ -32,22 +26,15 @@ import { HistoricalListComponent } from './items/historical-list/historical-list
     ItemComponent,
     ItemDetailsComponent,
     ConsumerComponent,
-<<<<<<< HEAD
-    MyStringComponent
+    TodoListComponent, 
+    MyStringComponent,
+    HistoricalListComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
-=======
-    TodoListComponent, 
-    HistoricalListComponent
-  ],
-  imports: [
-    BrowserModule,
     AppRoutingModule
->>>>>>> f9c905d0abb2f8a5280f5f5c08261545ee8fb6a2
   ],
   providers: [
     ItemListResolver
