7bf55920c4178156ce0ea4ead84a98264985119a
unknown <emilio.mst@gmail.com>
2018-07-28 00:36:34 -0500

    Replaced some bootstrap code with mdc layout grid

--- src/app/items/shared/item.model.ts
+++ src/app/items/shared/item.model.ts
@@ -1,4 +1,6 @@
 export interface IItem {
     id: number;
     name: string;
+    type: string;
+    items: any[];
 }
--- src/app/items/shared/list-item.model.ts
+++ src/app/items/shared/list-item.model.ts
@@ -3,6 +3,8 @@ import { IItem } from "./item.model";
 export class ListItem implements IItem{
     id: number;
     name: string;
+    type: string;
+    items: any[];
     constructor() {  
         this.name = "";
     }
